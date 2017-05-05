73960b4b29c06d0a86943104b2af9a67e0c183ba
bunkerrh <ryanhbunker@gmail.com>
2017-05-05 19:12:04 -0400

    Synched Thread
    Syched the thread so that the numbers appear in order. Testing the first
    100 numbers seems to have gone much faster although I did not do any
    testing and that is just based on my observation. 
    
    Counter seems to work and now it is a matter of letting the program run.
    
    TODO: research how many cores can be running effciently at the same time.
    Signed-off-by: bunkerrh <ryanhbunker@gmail.com>

--- BigintCounter/src/counter/Counter.java
+++ BigintCounter/src/counter/Counter.java
@@ -5,49 +5,52 @@ import java.math.BigInteger;
 class Counter {
 	BigInteger count = new BigInteger("0");
 	BigInteger one = new BigInteger("1");
-	BigInteger hsand = new BigInteger("100000000"); //I want to view the output of the counter however I do not need to see every number as this will slow down the program
-	//Even checking checking every 100,000 numbers will likely affect performance too much. The number we are trying toget to is 10^22,000,000. After verifying that programs works
-	//I will have to bump this number up quit high
-	
+	BigInteger hsand = new BigInteger("100000000"); // I want to view the output
+													// of the counter however I
+													// do not need to see every
+													// number as this will slow
+													// down the program
+	// Even checking checking every 100,000 numbers will likely affect
+	// performance too much. The number we are trying toget to is 10^22,000,000.
+	// After verifying that programs works
+	// I will have to bump this number up quit high
+
 	BigInteger zero = new BigInteger("0");
-	//The threads are syched so that the numbers do not appear out of order
+
+	// The threads are syched so that the numbers do not appear out of order
 	synchronized void printnumner() {
 		while (true) {
-			if(count.mod(hsand).equals(zero)){
-			System.out.println( count );
+			if (count.mod(hsand).equals(zero)) {
+				System.out.println(count);
 			}
 			count = count.add(one);
 
-			
-			}
 		}
 	}
+}
 
-	class myThread1 extends Thread{
-		Counter c;
-		myThread1(Counter c){
+// TODO: This seems tedious, if i want to add more threads i have to completely
+// change the code. Find a solution
+class myThread1 extends Thread {
+	Counter c;
+
+	myThread1(Counter c) {
 		this.c = c;
-		}
-		public void run(){
-			c.printnumner();
-		}
 	}
-	
-	class myThread2 extends Thread{
-		Counter c;
-		myThread2(Counter c){
+
+	public void run() {
+		c.printnumner();
+	}
+}
+
+class myThread2 extends Thread {
+	Counter c;
+
+	myThread2(Counter c) {
 		this.c = c;
-		}
-		public void run(){
-			c.printnumner();
-		}
 	}
-	
-	
-	
-	
 
-
-
-	
-
+	public void run() {
+		c.printnumner();
+	}
+}
